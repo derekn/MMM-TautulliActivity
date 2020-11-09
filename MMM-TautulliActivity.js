ed9e44fb2313c38001828aa2c5426072803c563a
Derek Nicol <1420397+derekn@users.noreply.github.com>
2020-11-09 11:52:39 -0600

    reuse existing reload timer to reduce requests with multiple clients

--- MMM-TautulliActivity.js
+++ MMM-TautulliActivity.js
@@ -19,6 +19,7 @@ Module.register('MMM-TautulliActivity', {
 			'buffering': 'far fa-sync-alt',
 		},
 	},
+	activityData: null,
 
 	getStyles: function() {
 		return [
@@ -31,26 +32,25 @@ Module.register('MMM-TautulliActivity', {
 
 		this.config.host = this.config.host.trim().replace(/\/$/, '')
 		this.config.updateFrequency = this.config.updateFrequency * 60 * 1000;
-		this.activity_data = null;
 
-		this.sendSocketNotification('GET_DATA', this.config);
+		this.sendSocketNotification('INIT', this.config);
 	},
 
 	getDom: function() {
 		var wrapper = document.createElement('div');
 		wrapper.className = 'small';
 
-		if (! this.activity_data) {
+		if (! this.activityData) {
 			wrapper.innerHTML = '<span class="loading dimmed">loading&hellip;</span>';
-		} else if (typeof this.activity_data === 'string') {
-			wrapper.innerHTML = `<span class="error">${this.activity_data}</span>`;
-		} else if (! this.activity_data.sessions.length) {
+		} else if (typeof this.activityData === 'string') {
+			wrapper.innerHTML = `<span class="error">${this.activityData}</span>`;
+		} else if (! this.activityData.sessions.length) {
 			if (this.config.hideOnNoActivity && ! this.hidden) {
 				this.hide();
 			}
 			wrapper.innerHTML = '<span class="no-activity dimmed">nothing is currently playing</span>';
 		} else {
-			for (const row of this.activity_data.sessions) {
+			for (const row of this.activityData.sessions) {
 				wrapper.innerHTML += `
 					<div class="activity-row ${row.state}" data-user-id="${row.user_id}">
 						<div class="activity">
@@ -71,7 +71,7 @@ Module.register('MMM-TautulliActivity', {
 
 	socketNotificationReceived: function(notification, payload) {
 		if (notification == 'SET_DATA') {
-			this.activity_data = payload;
+			this.activityData = payload;
 			this.updateDom(this.config.animationSpeed);
 		}
 	},
--- node_helper.js
+++ node_helper.js
@@ -2,13 +2,18 @@
  * MMM-TautulliActivity node helper
  */
 
-var NodeHelper = require('node_helper');
-var request = require('request');
+const NodeHelper = require('node_helper');
+const request = require('request');
 
 module.exports = NodeHelper.create({
-	getData: function(config) {
-		req = request.defaults({'agentOptions': {'rejectUnauthorized': false}});
-		req.get(`${config.host}/api/v2?apikey=${config.apiKey}&cmd=get_activity`, (err, res, body) => {
+	config: null,
+	data: null,
+	reloadTimer: null,
+
+	getData: function() {
+		console.info('MMM-TautulliActivity: fetching data');
+		var req = request.defaults({'agentOptions': {'rejectUnauthorized': false}});
+		req.get(`${this.config.host}/api/v2?apikey=${this.config.apiKey}&cmd=get_activity`, (err, res, body) => {
 			var data = JSON.parse(body);
 
 			if (err || res.statusCode != 200) {
@@ -18,14 +23,21 @@ module.exports = NodeHelper.create({
 				data = data.response.data;
 			}
 
-			this.sendSocketNotification('SET_DATA', data);
-			setTimeout(() => { this.getData(config) }, config.updateFrequency);
+			this.data = data;
+			this.sendSocketNotification('SET_DATA', this.data);
+			this.reloadTimer = setTimeout(() => { this.getData(this.config) }, this.config.updateFrequency);
 		});
 	},
 
 	socketNotificationReceived: function(notification, payload) {
-		if (notification == 'GET_DATA') {
-			this.getData(payload);
+		if (notification == 'INIT') {
+			if (! this.reloadTimer) {
+				console.info('MMM-TautulliActivity: starting reload timer');
+				this.config = payload;
+				this.getData();
+			} else {
+				this.sendSocketNotification('SET_DATA', this.data);
+			}
 		}
 	},
 });
