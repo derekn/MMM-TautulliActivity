2676ac9b1352002a3f1cb7dde479a074d021ed29
Derek Nicol <1420397+derekn@users.noreply.github.com>
2021-07-24 13:19:50 -0500

    ignoring unauthorized ssl certificates for self-signed certs

--- node_helper.js
+++ node_helper.js
@@ -4,6 +4,9 @@
 
 const NodeHelper = require('node_helper');
 const fetch = require('node-fetch');
+const https = require('https');
+
+const httpsAgent = new https.Agent({rejectUnauthorized: false});
 
 module.exports = NodeHelper.create({
 	config: null,
@@ -12,7 +15,7 @@ module.exports = NodeHelper.create({
 
 	getData: function() {
 		console.info('MMM-TautulliActivity: fetching data');
-		fetch(`${this.config.host}/api/v2?apikey=${this.config.apiKey}&cmd=get_activity`)
+		fetch(`${this.config.host}/api/v2?apikey=${this.config.apiKey}&cmd=get_activity`, {agent: httpsAgent})
 			.then((res) => {
 				if (res.ok) {
 					return res;
